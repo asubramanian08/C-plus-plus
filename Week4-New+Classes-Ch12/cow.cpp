9e795dcfe5e3ab2acc2ebf4d46a2536e5b6c83fa
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-02-15 20:48:57 -0500

    week 4 hw done

---
+++ Week4-New+Classes-Ch12/cow.cpp
@@ -0,0 +1,52 @@
+#include "cow.hpp"
+#include <cstring>
+#include <iostream>
+
+Cow::Cow()
+{
+    name[0] = '\0';
+    hobby = new char[1];
+    hobby[0] = '\0';
+    weight = 0;
+}
+Cow::Cow(const char *nm, const char *ho, double wt)
+{
+    int ho_size = strlen(ho) + 1;
+    hobby = new char[ho_size];
+    strncpy(hobby, ho, ho_size);
+    strncpy(name, nm, 20);
+    weight = wt;
+}
+Cow::Cow(const Cow &c)
+{
+    int ho_size = strlen(c.hobby) + 1;
+    hobby = new char[ho_size];
+    strncpy(hobby, c.hobby, ho_size);
+    strncpy(name, c.name, 20);
+    weight = c.weight;
+}
+Cow::~Cow()
+{
+    delete[] hobby;
+}
+
+Cow &Cow::operator=(const Cow &c)
+{
+    if (this != &c)
+    {
+        delete[] hobby;
+        int ho_size = strlen(c.hobby) + 1;
+        hobby = new char[ho_size];
+        strncpy(hobby, c.hobby, ho_size);
+        strncpy(name, c.name, 20);
+        weight = c.weight;
+    }
+    return *this;
+}
+
+void Cow::ShowCow() const
+{
+    std::cout << "The cows name is: " << name << std::endl
+              << name << "\'s hobby is: " << hobby << std::endl
+              << name << " weighs " << weight << " pounds" << std::endl;
+}
