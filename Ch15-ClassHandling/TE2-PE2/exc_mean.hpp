069c40a1d15402848990add0fce61d71656d468f
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-04-08 13:55:41 -0400

    week 16.1 class

--- Ch15-ClassHandling/TE2-PE2/exc_mean.hpp
+++ Ch15-ClassHandling/TE2-PE2/exc_mean.hpp
@@ -1,14 +1,15 @@
 #include <stdexcept>
+#include <string>
 
 class bad_hmean : public std::logic_error
 {
 public:
-    bad_hmean(const char *mesg = "hmean() -> the values entered add to zero")
-        : logic_error(mesg){};
+    bad_hmean(std::string f_nm = "hmean", std::string prob = "a is -b")
+        : logic_error(f_nm + ": " + prob){};
 };
 class bad_gmean : public std::logic_error
 {
 public:
-    bad_gmean(const char *mesg = "gmean() -> one or more of the values entered were negative")
-        : logic_error(mesg){};
+    bad_gmean(std::string f_nm = "gmean()", std::string prob = "a < 0 or b < 0")
+        : logic_error(f_nm + ": " + prob){};
 };
