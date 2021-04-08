ec8b1406b951b5837e4f41e9aa6c0ea514ff3e54
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-04-08 13:55:41 -0400

    week 16.1 class

--- Ch15-ClassHandling/TE3-PE3/exc_mean.hpp
+++ Ch15-ClassHandling/TE3-PE3/exc_mean.hpp
@@ -5,18 +5,19 @@ class bad_avg : public std::logic_error
 {
 public:
     bad_avg(std::string mesg, double x, double y)
-        : logic_error(mesg + std::string("\nValues are ") + std::to_string(x) +
-                      std::string(", ") + std::to_string(y)){};
+        : logic_error(mesg + "\nValues are " +
+                      std::to_string(x) + ", " +
+                      std::to_string(y)){};
 };
 class bad_hmean : public bad_avg
 {
 public:
-    bad_hmean(double x, double y, const char *mesg = "hmean() -> the values entered add to zero")
-        : bad_avg(mesg, x, y){};
+    bad_hmean(double x, double y, std::string f_nm = "hmean", std::string prob = "a is -b")
+        : bad_avg(f_nm + ": " + prob, x, y){};
 };
 class bad_gmean : public bad_avg
 {
 public:
-    bad_gmean(double x, double y, const char *mesg = "gmean() -> one or more of the values entered were negative")
-        : bad_avg(mesg, x, y){};
+    bad_gmean(double x, double y, std::string f_nm = "gmean()", std::string prob = "a < 0 or b < 0")
+        : bad_avg(f_nm + ": " + prob, x, y){};
 };
