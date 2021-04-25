63b5514ef3599a5af81fe71d030e45d14f077057
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-04-25 15:07:10 -0400

    16.c revise

--- Ch16-STL/C-Algorithms/TE4/main.cpp
+++ Ch16-STL/C-Algorithms/TE4/main.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 int main(void)
 {
-    vector<int> v1 = {1, 2, 3};
-    vector<int> v2;
+    vector<int> v1 = {1, 2, 3}, v2;
     transform(v1.begin(), v1.end(), back_inserter(v2), [](int x) { return x * 10; });
-    transform(v1.begin(), v1.end(), v2.begin(), plus<int>());
+    transform(v1.begin(), v1.end(), v2.begin(), v2.begin(), plus<int>());
     transform(v2.begin(), v2.end(), v2.begin(), negate<int>());
+
     string str = "hello world";
     transform(str.begin(), str.end(), str.begin(), toupper);
+
     vector<string> vs1{"book", "good", "civil"};
     vector<string> vs2{"worm", "bye", "war"};
-    vector<string> vs;
-    //?
+    vector<string> vs3;
+    transform(vs1.begin(), vs1.end(), vs2.begin(), back_inserter(vs3), plus<string>());
     return 0;
 }
