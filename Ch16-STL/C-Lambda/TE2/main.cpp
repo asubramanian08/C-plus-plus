080f6ae5e372eec716898385bf4c6a06075598a8
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-04-30 17:30:14 -0400

    16.c corrections

--- Ch16-STL/C-Lambda/TE2/main.cpp
+++ Ch16-STL/C-Lambda/TE2/main.cpp
@@ -2,18 +2,20 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <iterator>
 using namespace std;
 
 int main(void)
 {
     vector<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    remove_if(vi.begin(), vi.end(), [](int x) { return x % 3 != 0; }), vi.end();
+    vi.erase(remove_if(vi.begin(), vi.end(), [](int x) { return x % 3 != 0; }), vi.end());
 
-    cout << "Enter a sentence: ";
-    istream_iterator<string> in_str(cin);
+    cout << "Enter a sentence (add 2 spaces before enter): ";
     vector<string> vs;
-    //???
+    string input;
+    while (getline(cin, input, ' ') && !input.empty())
+        vs.push_back(input);
+    cin.clear();
+    cin.ignore();
     vs.erase(remove_if(vs.begin(), vs.end(), [](const string &str) { return str.size() > 4; }), vs.end());
     for (const string &str : vs)
         cout << str << ' ';
@@ -22,7 +24,7 @@ int main(void)
     cout << "Enter a string: ";
     string noSpace;
     getline(cin, noSpace);
-    noSpace.erase(remove_if(noSpace.begin(), noSpace.end(), [](char ch) { return isspace(ch) == 0; }));
+    noSpace.erase(remove_if(noSpace.begin(), noSpace.end(), [](char ch) { return isspace(ch) != 0; }), noSpace.end());
 
     vector<int> vi_3divs;
     remove_copy_if(vi.begin(), vi.end(), back_inserter(vi_3divs), [](int x) { return x % 3 == 0; });
--- Ch16-STL/C-Lambda/TE4/main.cpp
+++ Ch16-STL/C-Lambda/TE4/main.cpp
@@ -6,13 +6,13 @@ template <typename Iter, typename Func>
 void forEach(Iter start, Iter end, Func function)
 {
     for (auto curr = start; curr != end; curr++)
-        function(curr);
+        function(*curr);
 }
 
 int main(void)
 {
     vector<int> vi;
     vi.resize(20);
-    forEach(vi.begin(), vi.end(), [](vector<int>::iterator x) { *x = 1; });
+    forEach(vi.begin(), vi.end(), [](int &x) { x = 1; });
     return 0;
 }
